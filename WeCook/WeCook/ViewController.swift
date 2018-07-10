ae49544e7faf32dafb9f6f65b06ea6a00d826272
Bruno Pinheiro <brunopinheiro@MangaBook-Pro.local>
2018-07-10 20:40:25 -0300

    Create Xcode project and setup .gitignore

---
+++ WeCook/WeCook/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeCook
+//
+//  Created by Bruno Pinheiro on 7/8/18.
+//  Copyright © 2018 AppfelStudio. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
