bcf68e38244bcca43454f1209f129bf905b05472
Bruno Pinheiro <brunovapinheiro@gmail.com>
2018-07-10 21:39:53 -0300

    Create Recipes content controller UI (placeholder) and respective controller and view model

---
+++ WeCook/WeCook/Recipes/RecipesViewController.swift
@@ -0,0 +1,13 @@
+//
+// RecipesViewController.swift
+// WeCook
+//
+// Created on 7/10/18.
+// Copyright (c) 2018 AppfelStudio. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+  
+}
---
+++ WeCook/WeCook/Recipes/RecipesViewModel.swift
@@ -0,0 +1,13 @@
+//
+// RecipesViewModel.swift
+// WeCook
+//
+// Created on 7/10/18.
+// Copyright (c) 2018 AppfelStudio. All rights reserved.
+//
+
+import Foundation
+
+class RecipesViewModel {
+
+}
