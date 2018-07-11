76f7d9ddd9cdd97ec0693d66ef8e4d969b308e85
Bruno Pinheiro <brunovapinheiro@gmail.com>
2018-07-10 21:10:08 -0300

    Add icon image assets for the tabs and setup tab bar tint color

---
+++ WeCook/WeCook/Appearance/Color+WeCook.swift
@@ -0,0 +1,13 @@
+//
+// Color+WeCook.swift
+// WeCook
+//
+// Created on 7/10/18.
+// Copyright (c) 2018 AppfelStudio. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  static var themePrimary: UIColor = .red
+}
