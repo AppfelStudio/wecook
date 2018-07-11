9c669cfbf7c61dc334201f3f47b6dd22e6117c9d
Bruno Pinheiro <brunovapinheiro@gmail.com>
2018-07-10 21:17:41 -0300

    Update first tab content controller to be embedded in a Navigation Controller

--- WeCook/WeCook/AppDelegate.swift
+++ WeCook/WeCook/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-    UITabBar.appearance().tintColor = .themePrimary
+    window?.tintColor = .themePrimary
 
     return true
   }
